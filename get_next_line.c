382fe7e30b319586edcf2659914fac244ac58d60
Daryna Serhieieva <dserhiei@c3r1s3.42urduliz.com>
2024-07-31 21:36:18 +0200

    refactored my code and change from 4 function to 5

--- get_next_line.c
+++ get_next_line.c
@@ -10,85 +10,63 @@ static char	*read_fd(int fd)
 		return (NULL);
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read <= 0)
-		return (free(buffer), NULL);
-	else
-		buffer[bytes_read] = '\0';
+	{
+		free(buffer);
+		return (NULL);
+	}
+	buffer[bytes_read] = '\0';
 	return (buffer);
 }
 
-static char	*new_line(char **leftover)
+static char	*extract_line(char **leftover, char *newline)
 {
-	char	*newline;
 	char	*line;
-	char	*tmp;
 	size_t	line_length;
+
+	line_length = newline + 1 - *leftover;
+	line = (char *)malloc(line_length + 1);
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, *leftover, line_length + 1);
+	return (line);
+}
+
+static void	update_leftover(char **leftover, char *newline)
+{
+	char	*tmp;
 	size_t	leftover_length;
-	
-	newline = ft_strchr(*leftover, '\n');
-	if (newline)
+
+	leftover_length = ft_strlen(newline + 1);
+	if (leftover_length > 0)
 	{
-		line_length = newline + 1 - *leftover;
-		line = (char *)malloc(line_length + 1);
-		if (!line)
-			return (NULL);
-		ft_strlcpy(line, *leftover, line_length + 1);
-		leftover_length = ft_strlen(newline + 1);
-		if (leftover_length > 0)
+		tmp = (char *)malloc(leftover_length + 1);
+		if (tmp)
 		{
-			tmp = (char *)malloc(leftover_length + 1);
-			if (!tmp)
-			{
-				free(line);
-				return (NULL);
-			}
 			ft_strlcpy(tmp, newline + 1, leftover_length + 1);
 			free(*leftover);
 			*leftover = tmp;
 		}
-		else
-		{
-			free(*leftover);
-			*leftover = NULL;
-		}
-		return (line);
 	}
-	return (NULL);
+	else
+	{
+		free(*leftover);
+		*leftover = NULL;
+	}
 }
 
-static char	*get_next(int fd, char **leftover)
+static char	*process_leftover(char **leftover)
 {
-	char	*line = NULL;
-	char	*tmp;
-	char	*buffer = NULL;
+	char	*newline;
+	char	*line;
 
-	buffer = read_fd(fd);
-	if (!buffer)
-		return (NULL);
-	while (buffer)
+	newline = ft_strchr(*leftover, '\n');
+	line = NULL;
+	if (newline)
 	{
-		if (*leftover)
-		{
-			tmp = ft_strjoin(*leftover, buffer); 
-			free(buffer);
-			free(*leftover);
-			if (!tmp)
-				return (NULL);
-			*leftover = tmp;
-		}
-		else
-		{
-			*leftover = ft_strdup(buffer);
-			free(buffer);
-			if (!*leftover)
-				return (NULL);
-		}
-		if (ft_strchr(*leftover, '\n'))
-			break;
-		buffer = read_fd(fd);
+		line = extract_line(leftover, newline);
+		update_leftover(leftover, newline);
 	}
-	if (ft_strchr(*leftover, '\n'))
-		line = new_line(leftover);
-	else
+	else if (*leftover)
 	{
 		line = ft_strdup(*leftover);
 		free(*leftover);
@@ -100,19 +78,159 @@ static char	*get_next(int fd, char **leftover)
 char	*get_next_line(int fd)
 {
 	static char	*leftover = NULL;
-	char	*line = NULL;
+	char		*line;
+	char		*buffer;
+	char		*tmp;
 
+	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!leftover || !ft_strchr(leftover, '\n'))
+	buffer = read_fd(fd);
+	while (!line && buffer)
 	{
-		line = get_next(fd, &leftover);
-		if (!line && !leftover)
+		if (leftover)
+			tmp = ft_strjoin(leftover, buffer);
+		else
+			tmp = ft_strdup(buffer);
+		free(buffer);
+		if (!tmp)
 			return (NULL);
-		if (!line && leftover)
-			line = new_line(&leftover);
+		free(leftover);
+		leftover = tmp;
+		if (ft_strchr(leftover, '\n'))
+			break ;
+		buffer = read_fd(fd);
 	}
-	else
-		line = new_line(&leftover);
+	if (leftover)
+		line = process_leftover(&leftover);
 	return (line);
 }
+
+// #include "get_next_line.h"
+
+// static char	*read_fd(int fd)
+// {
+// 	ssize_t	bytes_read;
+// 	char	*buffer;
+
+// 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 	if (!buffer)
+// 		return (NULL);
+// 	bytes_read = read(fd, buffer, BUFFER_SIZE);
+// 	if (bytes_read <= 0)
+// 		return (free(buffer), NULL);
+// 	else
+// 		buffer[bytes_read] = '\0';
+// 	return (buffer);
+// }
+
+// static char	*new_line(char **leftover)
+// {
+// 	char	*newline;
+// 	char	*line;
+// 	char	*tmp;
+// 	size_t	line_length;
+// 	size_t	leftover_length;
+
+// 	newline = ft_strchr(*leftover, '\n');
+// 	if (newline)
+// 	{
+// 		line_length = newline + 1 - *leftover;
+// 		line = (char *)malloc(line_length + 1);
+// 		if (!line)
+// 			return (NULL);
+// 		ft_strlcpy(line, *leftover, line_length + 1);
+// 		leftover_length = ft_strlen(newline + 1);
+// 		if (leftover_length > 0)
+// 		{
+// 			tmp = (char *)malloc(leftover_length + 1);
+// 			if (!tmp)
+// 			{
+// 				free(line);
+// 				return (NULL);
+// 			}
+// 			ft_strlcpy(tmp, newline + 1, leftover_length + 1);
+// 			free(*leftover);
+// 			*leftover = tmp;
+// 		}
+// 		else
+// 		{
+// 			free(*leftover);
+// 			*leftover = NULL;
+// 		}
+// 		return (line);
+// 	}
+// 	if (!newline && *leftover)
+// 	{
+// 		line = ft_strdup(*leftover);
+// 		free(*leftover);
+// 		*leftover = NULL;
+// 		return (line);
+// 	}
+// 	return (NULL);
+// }
+
+// static char	*get_next(int fd, char **leftover)
+// {
+// 	char	*line;
+// 	char	*tmp;
+// 	char	*buffer;
+
+// 	line = NULL;
+// 	buffer = NULL;
+// 	buffer = read_fd(fd);
+// 	if (!buffer)
+// 		return (NULL);
+// 	while (buffer)
+// 	{
+// 		if (*leftover)
+// 		{
+// 			tmp = ft_strjoin(*leftover, buffer);
+// 			free(buffer);
+// 			free(*leftover);
+// 			if (!tmp)
+// 				return (NULL);
+// 			*leftover = tmp;
+// 		}
+// 		else
+// 		{
+// 			*leftover = ft_strdup(buffer);
+// 			free(buffer);
+// 			if (!*leftover)
+// 				return (NULL);
+// 		}
+// 		if (ft_strchr(*leftover, '\n'))
+// 			break ;
+// 		buffer = read_fd(fd);
+// 	}
+// 	if (ft_strchr(*leftover, '\n'))
+// 		line = new_line(leftover);
+// 	else
+// 	{
+// 		line = ft_strdup(*leftover);
+// 		free(*leftover);
+// 		*leftover = NULL;
+// 	}
+// 	return (line);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	*leftover = NULL;
+// 	char		*line;
+
+// 	line = NULL;
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+// 	if (!leftover || !ft_strchr(leftover, '\n'))
+// 	{
+// 		line = get_next(fd, &leftover);
+// 		if (!line && !leftover)
+// 			return (NULL);
+// 		if (!line && leftover)
+// 			line = new_line(&leftover);
+// 	}
+// 	else
+// 		line = new_line(&leftover);
+// 	return (line);
+// }
